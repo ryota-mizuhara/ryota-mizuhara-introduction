958aebf63a8c96c384bdfb2d0510dfd89738c0d9
ryota-mizuhara <ryota.mizuhara@force-corp.co.jp>
2021-05-26 10:06:06 +0900

    Web site

---
+++ self-introduction.js
@@ -0,0 +1,27 @@
+'use strict'
+
+$('.slider').slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
+    responsive: [
+        {
+        breakpoint: 769,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+        }
+    },
+    {
+        breakpoint: 426,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+    }
+]
+});
